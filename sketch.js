96ed07600c2d71608fc489c2821c4b21bb88e1ab
Hartaj400 <72198741+Hartaj400@users.noreply.github.com>
2020-12-01 08:43:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -45,6 +45,13 @@ function draw(){
   
  fill(255,255,254);
  textSize(15);
+ if(lastFed>=12){
+   text("Last Feed:"+lastFed%12+ "PM",350,30);
+ }else if(lastFed==0){
+   text("Last Feed: 12 AM",350,30);
+ }else{
+   text("Last Feed: "+ lastFed + "AM",350,30);
+ }
 
 drawSprites();
 }
@@ -70,9 +77,9 @@ function writePosition(nazo){
 
 }
 function AddFood(){
-position++
+foodS++
 database.ref('/').update({
-  Food:position
+  Food:foodS
 }
 
 )
